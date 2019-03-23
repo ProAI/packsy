bc187ca549d4aa035f259c7447734b3735015382
Markus J. Wetzel <markuswetzel@gmx.net>
2019-03-23 10:51:50 +0100

    Fix lint-staged config

--- src/config/lint-staged.js
+++ src/config/lint-staged.js
@@ -12,9 +12,9 @@ const nonScripts = Object.values(fileExtensions)
 module.exports = {
   linters: {
     // Format and lint all javascript/typescript files.
-    [`*.+(${scripts})`]: [`${bin} format`, `${bin} lint`, 'git add'],
+    [`*.+(${scripts})`]: [`node ${bin} format`, `node ${bin} lint`, 'git add'],
     // Format only all other files.
-    [`*.+(${nonScripts})`]: [`${bin} format`, 'git add'],
+    [`*.+(${nonScripts})`]: [`node ${bin} format`, 'git add'],
   },
   // Ignore all dot files.
   ignore: ['.*'],
